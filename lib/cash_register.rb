2ac2bcd1d82a4148f7fa13febf53148a7ff95dbd
Onur Eker <yonureker@gmail.com>
2018-01-15 09:30:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
 
+  attr_accessor :discount, :total
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+  end
+
+  def apply_discount
+  end
+
+  def items
+  end
+
+  def void_last_transaction
+  end
+
+end
